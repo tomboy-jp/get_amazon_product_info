4291f802c0c2def0a850389c0312a58c364b5141
tomboy <tomboy-jp@users.noreply.github.com>
2018-05-01 16:54:27 +0900

    update

--- get_item.py
+++ get_item.py
@@ -2,14 +2,14 @@ import os
 import time
 from amazon.api import AmazonAPI
 
-def amazon_product_info(ItemId, trying=3):
+# Amazonアソシエイトのアカウント取得後、下記の3パラメータを環境変数に書き込んでください。
+AMAZON_ACCESS_KEY = os.environ['AMAZON_ACCESS_KEY']
+AMAZON_SECRET_KEY = os.environ['AMAZON_SECRET_KEY']
+AMAZON_ASSOCIATE_TAG = os.environ['AMAZON_ASSOCIATE_TAG']
 
-    # Amazonアソシエイトのアカウント取得後、下記の3パラメータを環境変数に書き込んでください。
-    AMAZON_ACCESS_KEY = os.environ['AMAZON_ACCESS_KEY']
-    AMAZON_SECRET_KEY = os.environ['AMAZON_SECRET_KEY']
-    AMAZON_ASSOCIATE_TAG = os.environ['AMAZON_ASSOCIATE_TAG']
+amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOCIATE_TAG, Region='JP')
 
-    amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOCIATE_TAG, Region='JP')
+def from_code(ItemId, trying=3):
 
     cnt = 0
 
@@ -17,14 +17,6 @@ def amazon_product_info(ItemId, trying=3):
 
         try:
             product = amazon.lookup(ItemId=ItemId)
-            print("商品名:\t\t{}".format(product.title))
-            print("価格:\t\t{}".format(product.formatted_price))
-            print("ジャンル:\t{}".format(product.genre))
-            print("ランキング:\t{}".format(product.sales_rank))
-
-            # 他のメタデータを参照する場合。
-            # print(dir(product))
-
             return product
 
         except:
@@ -33,5 +25,27 @@ def amazon_product_info(ItemId, trying=3):
         cnt += 1
         time.sleep(1)
 
-#asinコード or 商品コードを指定して呼び出す。
-amazon_product_info("4873118360")
+
+def printer(product):
+
+    print("商品名:\t\t{}".format(product.title))
+    print("価格:\t\t{}".format(product.formatted_price))
+    print("ジャンル:\t{}".format(product.genre))
+    print("ランキング:\t{}".format(product.sales_rank))
+    print("商品URL:\t{}".format(product.offer_url))
+    print("画像URL:\t{}".format(product.medium_image_url))
+    print("")
+
+    # 他のメタデータを参照する場合。
+    # print(dir(product))
+
+#asinコード or 商品コードのリスト。
+list = ["4873118360",  "B00PVHO6O8", "B000TUEUZW", "B005VB4IGA", "B07BBZ4YJ9"]
+
+for i in list:
+
+    product = from_code(i, 10)
+    try:
+        printer(product)
+    except:
+        pass
